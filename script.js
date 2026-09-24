b7b151cd036094fc42e72e50b47090464131c141
Katarina Bloch <katarina.bloch@mymail.champlain.edu>
2026-09-24 14:37:05 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+const backToTopButton = document.getElementById('back-to-top');
+
+if (backToTopButton) {
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 800) { // was 400
+        backToTopButton.classList.add('visible');
+    } else {
+        backToTopButton.classList.remove('visible');
+    }
+  });
+
+  backToTopButton.addEventListener('click', () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  });
+}
